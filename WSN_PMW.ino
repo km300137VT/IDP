96f9460d5c41433f953f636494763c28c306502c
katherin Marroquin <142833749+km300137VT@users.noreply.github.com>
2024-03-24 20:28:37 -0400

    Implemented temperarure sensor. The values need adjusting to get correct reading  but the code it seld works.

--- WSN_PMW.ino
+++ WSN_PMW.ino
@@ -1,10 +1,20 @@
-#define PWM_PIN 9
+﻿#define PWM_PIN 9
 #define SUPPLY_VOLTAGE_PIN A0
 
 #define CLOCK_FREQUENCY 16000000
 #define PWM_FREQUENCY 50000
-
+//PMW signal global variables
 double dutyCycle = 0.61;
+
+//Thermitsor global variables
+#define ntc_pin A0         // Pin, to which the voltage divider is connected
+#define vd_power_pin 2        // 5V for the voltage divider
+#define nominal_resistance 10000       //Nominal resistance at 25⁰C
+#define nominal_temeprature 25   // temperature for nominal resistance (almost always 25⁰ C)
+#define samplingrate 5    // Number of samples
+#define beta 3428  // The beta coefficient or the B value of the thermistor (usually 3000-4000) check the datasheet for the accurate value.
+#define Rref 10000   //Value of  resistor used for the voltage divider
+int samples = 0;   //array to store the samples
 
 void setup() {
   pinMode(SUPPLY_VOLTAGE_PIN, INPUT);
@@ -35,6 +45,10 @@ void setup() {
   Thus, the frequency can be calculated by 16,000,000 (the clock frequency) / ICR1 / 2 (since it counts up and then back down instead of resetting at the top)
   and the duty cycle can be calculated by OCR1A / ICR1 (threshold divided by top value)
   */
+
+  ///THERMITOR CODE
+pinMode(vd_power_pin,OUTPUT);
+ Serial.begin(9600);  // initialize serial communication at a baud rate of 9600
 }
 
 int counter = 0;
@@ -47,4 +61,39 @@ void loop() {
   counter += increment;
 
   delay(25);
+
+ uint8_t i;
+  float average;
+  samples = 0;
+
+  // take voltage readings from the voltage divider
+  digitalWrite(vd_power_pin, HIGH);
+  for (i = 0; i < samplingrate; i++) {
+    samples += analogRead(ntc_pin);
+    delay(10);
+  }
+
+  digitalWrite(vd_power_pin, LOW);
+  average = 0;
+  average = samples / samplingrate;
+  Serial.println("\n \n");
+  Serial.print("ADC readings ");
+  Serial.println(average);
+
+  // Calculate NTC resistance
+  average = 1023 / average - 1;
+  average = Rref / average;
+  Serial.print("Thermistor resistance ");
+  Serial.println(average);
+  float temperature;
+  temperature = average / nominal_resistance;     // (R/Ro)
+  temperature = log(temperature);                  // ln(R/Ro)
+  temperature /= beta;                   // 1/B * ln(R/Ro)
+  temperature += 1.0 / (nominal_temeprature + 273.15); // + (1/To)
+  temperature = 1.0 / temperature;                 // Invert
+  temperature -= 273.15;                         // convert absolute temp to C
+  Serial.print("Temperature ");
+  Serial.print(temperature);
+  Serial.println(" *C");
+  delay(2000);
 }
