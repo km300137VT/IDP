dadafff428a258724dffd93750430c10028c3fea
Cian Capacci <Cian.capacci@gmail.com>
2024-03-31 19:40:54 -0400

    FUNCTIONAL FEEDBACK SYSTEM WOOOOOOOOOOOO

--- WSN_PWM/WSN_PWM.ino
+++ WSN_PWM/WSN_PWM.ino
@@ -1,18 +1,22 @@
 #define PWM_PIN 9
 #define SUPPLY_VOLTAGE_PIN A1
-#define OUTPUT_VOLTAGE_PIN A0
+#define OUTPUT_VOLTAGE_PIN A5
 
 #define CLOCK_FREQUENCY 16000000
 #define PWM_FREQUENCY 50000
 
-#define OUTPUT_VOLTAGE_DIVIDER_SCALE (330000 + 680000) / (330000)
+#define SUPPLY_VOLTAGE_ACCEPTABLE_MINIMUM 3.0
+#define OUTPUT_VOLTAGE_DIVIDER_SCALE 3.43      //Should be *roughly* (330000 + 680000) / 330000 or whatever other voltage divider you use
 #define OUTPUT_VOLTAGE_ACCEPTABLE_ERROR 0.05
-#define PWM_KP 0.0005
+#define PWM_KP 0.005
+
+#define MIN_DUTY_CYCLE 0
+#define MAX_DUTY_CYCLE 1
 
 //PMW signal global variables
 double dutyCycle;
-double voltageTarget = 10;  //Initial, state machine implementation later
-const int numOutputVoltageSamples = 25;
+double voltageTarget = 10.0;  //Initial, state machine implementation later
+const int numOutputVoltageSamples = 5;
 double voltageOutputSamples[numOutputVoltageSamples];
 
 //Thermistor global variables
@@ -26,12 +30,26 @@ double voltageOutputSamples[numOutputVoltageSamples];
 int samples = 0;                  // array to store the samples
 
 void setup() {
+  Serial.begin(9600);  // initialize serial communication at a baud rate of 9600
+
   pinMode(SUPPLY_VOLTAGE_PIN, INPUT);
   pinMode(OUTPUT_VOLTAGE_PIN, INPUT);
   pinMode(PWM_PIN, OUTPUT);
 
   //Do preliminary duty cycle setting, will be close loop incremented towards more accurately during runtime
-  dutyCycle = -analogRead(SUPPLY_VOLTAGE_PIN)/voltageTarget + 1;
+  double supplyVoltage = analogRead(SUPPLY_VOLTAGE_PIN) / 1023.0f * 5.0f;
+  if (supplyVoltage > SUPPLY_VOLTAGE_ACCEPTABLE_MINIMUM) {
+    dutyCycle = (voltageTarget - supplyVoltage) / voltageTarget;
+  } else {
+    dutyCycle = 0;
+    Serial.println("SUPPLY TOO LOW, CONVERTER DISABLED");
+  }
+  
+  Serial.print("SUPPLY VOLTAGE: ");
+  Serial.println(supplyVoltage);
+  Serial.print("STARTING DUTY CYCLE: ");
+  Serial.println(dutyCycle);
+
 
   //COM1A1 selects output comparison mode
   TCCR1A = _BV(COM1A1) | _BV(WGM11); 
@@ -64,7 +82,6 @@ void setup() {
 
   ///THERMISTOR CODE
   pinMode(vd_power_pin, OUTPUT);
-  Serial.begin(9600);  // initialize serial communication at a baud rate of 9600
 }
 
 // int counter = 0;
@@ -77,7 +94,7 @@ void loop() {
   // if (counter == 70) increment = -1;
   // if (counter == 50) increment = 1;
   // counter += increment;
-  int voltageSampleSum = 0;
+  double voltageSampleSum = 0;
   bool voltageReadingsInitialized = true;
   for (int j = numOutputVoltageSamples - 1; j > 0; j--) {
     voltageOutputSamples[j] = voltageOutputSamples[j-1];                    //Shift all voltage readings one index right, deleting oldest value
@@ -86,51 +103,67 @@ void loop() {
   }
   voltageOutputSamples[0] = analogRead(OUTPUT_VOLTAGE_PIN);
   voltageSampleSum += voltageOutputSamples[0];
-  Serial.print("Current Voltage Sample Sum: ");
-  Serial.println(dutyCycle);
+  // Serial.print("Current Duty Cycle: ");
+  // Serial.println(dutyCycle);
 
   if (voltageReadingsInitialized) {   //Only start modifying stuff once the average is set, gives time for steady state response to stabilize
-    int outputVoltage = (voltageSampleSum/numOutputVoltageSamples) * OUTPUT_VOLTAGE_DIVIDER_SCALE / 1023 * 5;
-    int voltageError = outputVoltage - voltageTarget;
+    double outputVoltage = (voltageSampleSum/numOutputVoltageSamples) * OUTPUT_VOLTAGE_DIVIDER_SCALE / 1023.0f * 5.0f;
+    double voltageError = voltageTarget - outputVoltage;
+
+    Serial.print("Current Output Voltage: ");
+    Serial.print(outputVoltage);
+    Serial.print(" | Current Voltage Error: ");
+    Serial.print(voltageError);
+
     if (voltageError > OUTPUT_VOLTAGE_ACCEPTABLE_ERROR || voltageError < -OUTPUT_VOLTAGE_ACCEPTABLE_ERROR) {
-      dutyCycle += voltageError * PWM_KP;
-      OCR1A = ICR1 * dutyCycle;
+      if (dutyCycle + PWM_KP * voltageError > MIN_DUTY_CYCLE && dutyCycle + PWM_KP * voltageError < MAX_DUTY_CYCLE) {
+        dutyCycle += voltageError * PWM_KP;
+        OCR1A = ICR1 * dutyCycle;
+        
+        Serial.print(" | Adjusting duty cycle to: ");
+        Serial.println(dutyCycle);
+      } else {
+        Serial.println(" | Converter tried to adjust duty cycle but fell outside bounds.");
+      }
+    } else {
+      Serial.println(" | Converter output nominal.");
     }
   }
   delay(25);
+  Serial.println("");
 
-  uint8_t i;
-  float average;
-  samples = 0;
+  // uint8_t i;
+  // float average;
+  // samples = 0;
 
-  // take voltage readings from the voltage divider
-  digitalWrite(vd_power_pin, HIGH);
-  for (i = 0; i < samplingrate; i++) {
-    samples += analogRead(ntc_pin);
-    delay(10);
-  }
+  // // take voltage readings from the voltage divider
+  // digitalWrite(vd_power_pin, HIGH);
+  // for (i = 0; i < samplingrate; i++) {
+  //   samples += analogRead(ntc_pin);
+  //   delay(10);
+  // }
 
-  digitalWrite(vd_power_pin, LOW);
-  average = 0;
-  average = samples / samplingrate;
-  Serial.println("\n \n");
-  Serial.print("ADC readings ");
-  Serial.println(average);
+  // digitalWrite(vd_power_pin, LOW);
+  // average = 0;
+  // average = samples / samplingrate;
+  // Serial.println("\n \n");
+  // Serial.print("ADC readings ");
+  // Serial.println(average);
 
-  // Calculate NTC resistance
-  average = samples / samplingrate;
-  average = Rref * (1023.0 / average - 1.0);
-  Serial.print("Thermistor resistance ");
-  Serial.println(average);
-  float temperature;
-  temperature = average / nominal_resistance;     // (R/Ro)
-  temperature = log(temperature);                  // ln(R/Ro)
-  temperature /= beta;                   // 1/B * ln(R/Ro)
-  temperature += 1.0 / (nominal_temeprature + 273.15); // + (1/To)
-  temperature = 1.0 / temperature;                 // Invert
-  temperature -= 273.15;                         // convert absolute temp to C
-  Serial.print("Temperature ");
-  Serial.print(temperature);
-  Serial.println(" *C");
-  delay(2000);
+  // // Calculate NTC resistance
+  // average = samples / samplingrate;
+  // average = Rref * (1023.0 / average - 1.0);
+  // Serial.print("Thermistor resistance ");
+  // Serial.println(average);
+  // float temperature;
+  // temperature = average / nominal_resistance;     // (R/Ro)
+  // temperature = log(temperature);                  // ln(R/Ro)
+  // temperature /= beta;                   // 1/B * ln(R/Ro)
+  // temperature += 1.0 / (nominal_temeprature + 273.15); // + (1/To)
+  // temperature = 1.0 / temperature;                 // Invert
+  // temperature -= 273.15;                         // convert absolute temp to C
+  // Serial.print("Temperature ");
+  // Serial.print(temperature);
+  // Serial.println(" *C");
+  // delay(2000);
 }
